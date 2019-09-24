9313bbabec5456e9723b3e43ef80dbb7a0fd9523
erfffff <dakevcraft@yahoo.fr>
2019-09-24 07:31:08 +0100

    connect bdd and get data

--- src/main/java/decrypt_java/bdd.java
+++ src/main/java/decrypt_java/bdd.java
@@ -1,9 +1,22 @@
 package decrypt_java;
+import java.sql.*;
 
 public class bdd
 {	
-	public static void bddmsg()
+	public static void bdd()
 	{
-		System.out.println("salut");
+		try {
+			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/projet_decrypt?serverTimezone=UTC", "root", "test123*");
+			Statement myStmt = connect.createStatement();
+			ResultSet myRequest = myStmt.executeQuery("select * from log");
+			
+			while (myRequest.next()) {
+				System.out.println(myRequest.getString("id") + ", " + myRequest.getString("username") + ", " + myRequest.getString("password"));
+			}
+		}
+		catch (Exception exc) {
+			// TODO: handle exception
+			exc.printStackTrace();
+		}
 	}
 }
