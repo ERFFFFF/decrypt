6740b3291932a25bc83ad654da6482cf0d88f67e
Reyvenk <cedric.berterreche@hotmail.fr>
2019-09-24 11:29:00 +0200

    Creation des test ctrlCrypt

---
+++ src/test/java/decrypt_java/CLctrlCryptTest.java
@@ -0,0 +1,24 @@
+package decrypt_java;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class CLctrlCryptTest {
+
+	@Test
+	public void testEcritureLecture() throws IOException {
+		CLctrlCrypt.ecrireFichierSimple("C:\\Users\\cedri\\Desktop\\a.txt", "Bonjour a tous. Comment Allez vous?");
+		assertEquals("Bonjour a tous. Comment Allez vous?", CLctrlCrypt.lireFichierSimple("C:\\Users\\cedri\\Desktop\\a.txt"));
+	}
+	
+	@Test
+	public void testCryptage() throws IOException {
+		String mot ="Bonjour a tous. Comment Allez vous?" ;
+		String key = "key";
+		String motCrypte = CLctrlCrypt.crypter(mot, key);
+		assertEquals("Bonjour a tous. Comment Allez vous?", CLctrlCrypt.crypter(motCrypte, key));
+	}
+}
